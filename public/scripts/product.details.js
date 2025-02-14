c1d1d93f727132e122b221721c3a98b73df51337
Davit <davit.ners02@gmail.com>
2025-02-14 14:30:25 +0100

    Toggle function to show add comment section

--- public/scripts/product.details.js
+++ public/scripts/product.details.js
@@ -1,5 +1,13 @@
 const showFormBtn = document.getElementById('showCommentForm');
+const addCommentForm = document.querySelector('.commentForm');
+let show = false;
 
 showFormBtn.addEventListener('click', function() {
-    alert();
+    addCommentForm.classList.toggle('hidden');
+    show = !show;
+
+    if (show) showFormBtn.textContent = 'X';
+    else {
+        showFormBtn.textContent = 'Ajouter un commentaire';
+    }
 });
