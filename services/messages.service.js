1e36537136e5a0264983c747abdb5b3d49473523
Davit <davit.ners02@gmail.com>
2025-03-03 14:39:43 +0100

    Rajout titre + style messages

--- services/messages.service.js
+++ services/messages.service.js
@@ -4,8 +4,9 @@ const messageService = {
 
     getAll: async () => {
         const messages = await messagesModel.getAll();
-        const mapedMessages = messages.map(({ id, userEmail, checked, receivedDate, category }) => ({
+        const mapedMessages = messages.map(({ id, title, userEmail, checked, receivedDate, category }) => ({
             id,
+            title,
             userEmail,
             etat: checked ? 'Lu' : 'Non lu',
             receivedDate: new Date(receivedDate).toLocaleDateString("fr-FR"),
